6f1005040546653ee7f2ac6ed218fcfd216a5606
destinybbrianna <destinybbrianna@gmail.com>
2018-08-08 14:22:51 +0000

    automatically backed up by learn

--- dancing.rb
+++ dancing.rb
@@ -1,12 +1,7 @@
 #write a while loop
 #output step to the left"/ "step to right" 5 time
 
-counter = 0
-while counter < 5
-puts "steps to the left"
-puts "step to right"
-counter = counter + 1
-end
+
 
 def shake_it_off
 play_counter = 0 
