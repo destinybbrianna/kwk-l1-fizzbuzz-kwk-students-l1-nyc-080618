76ff0d34f2c1fb9e9aa8b8eacf72ffeb0d230e3b
destinybbrianna <destinybbrianna@gmail.com>
2018-08-08 13:59:16 +0000

    automatically backed up by learn

--- dancing.rb
+++ dancing.rb
@@ -1,5 +1,7 @@
 #write a while loop
 #output step to the left"/ "step to right" 5 time
 
-counter = 5 
-while counter 
+counter = 0
+while counter < 5
+puts "steps to the left"
+puts "step to right"
